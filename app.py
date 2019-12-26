00477f0bf47b26afa82163ba4e87393cb22a9c95
chanasit <kai.chr160@gmail.com>
2019-12-26 22:58:02 +0700

    make it better

--- app.py
+++ app.py
@@ -11,13 +11,34 @@ def handle_my_custom_namespace_event(json):
     print('test: received json: ' + str(json))
 
     # TODO: database query +++++
-    
+
     send("hello, world")
 
+
+
+
+@socketio.on('json', namespace='/text')
+def handle_json(json):
+    print('received json: ' + str(json))
+    send(json)
+
+
+@socketio.on('update', namespace='/text')
+def handle_json(update):
+    print('received json: ' + str(update))
+    send(update)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route("/update", methods=["POST"])
+def updated():
+    socketio.send("update complete")
+    return 
+
+
 
 
 if __name__ == '__main__':
