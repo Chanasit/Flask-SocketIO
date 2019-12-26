1dbef6a892dbc2d18b64877a83cb99f704fe5831
chanasit <kai.chr160@gmail.com>
2019-12-26 22:12:53 +0700

    make it better

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-@socketio.on('message')
-def handle_message(message):
-    print('received message: ' + message)
-
-
-@socketio.on('json')
-def handle_json(json):
-    print('received json: ' + str(json))
-
-
-@socketio.on('my event')
-def handle_my_custom_event(json):
-    print('received json: ' + str(json))
-
 
 @socketio.on('my event', namespace='/test')
 def handle_my_custom_namespace_event(json):
-    print('received json: ' + str(json))
+    print('test: received json: ' + str(json))
+
+    # TODO: database query +++++
+    
+    send("hello, world")
+
 
 @app.route('/')
 def home():
